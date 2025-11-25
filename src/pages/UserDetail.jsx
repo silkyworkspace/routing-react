1b4d58c969f205c0f4fef4263f55aef63e352575
“sawa” <silkyworkspace@gmail.com>
2025-11-25 16:30:26 +0900

    userリストをjsに分離

--- src/pages/UserDetail.jsx
+++ src/pages/UserDetail.jsx
@@ -1,11 +1,12 @@
 // src/pages/UserDetail.jsx
 import { useParams } from "react-router-dom";
+import { usersLists } from "./usersList.js";
 
 export default function UserDetail() {
-    const users = {
-        1: { name: "山田" },
-        2: { name: "佐藤" },
-    };
+    // const users = {
+    //     1: { name: "山田" },
+    //     2: { name: "佐藤" },
+    // };
 
     // useParams() は、ルートの :id 部分をオブジェクトで返す。
     // 例：/users/1 なら、{ id: "1" }を返す
@@ -14,7 +15,7 @@ export default function UserDetail() {
     // これは URL から取れた id をキーとして users オブジェクトから該当ユーザーを取り出しているということ。
     // URL が /users/1 → users["1"] → { name: "山田" }
     // URL が /users/2 → users["2"] → { name: "佐藤" }
-    const user = users[id];
+    const user = usersLists[id];
 
     return (
         <div>
---
+++ src/pages/usersList.js
@@ -0,0 +1,4 @@
+export const usersLists = {
+    1: { name: "山田" },
+    2: { name: "佐藤" },
+};
